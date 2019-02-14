ebdcc7c8f2e6ed48a4c63bb4f30bf20a3e4d340f
tankisov <tankisov.tahir@mail.ru>
2019-02-15 00:10:26 +0600

    dfgh

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -10,7 +10,48 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hekko");
+            int n = Convert.ToInt32(Console.ReadLine());// массив из n чисел 
+            int[] arr = new int[n]; // создал новый массив
+            for(int i=0; i < n; i++) 
+            {
+                arr [i] = Convert.ToInt32(Console.ReadLine()); // заполняю массив числами
+            }
+            //-------------------------------------------------
+            int[] arr1 = new int[n];
+            int b = 0;
+            for (int i=0;i<n;i++)
+            {
+                int cnt = 0;
+                if (arr[i] > 1)
+                {
+                    for(int j = 2; j < arr[i]; i++)
+                    {
+                        if(arr[i] % j != 0)
+                        {
+                            continue;
+                        }
+                        else {
+                            cnt++;
+                        }
+                        if (cnt == 0)
+                        {
+                            arr1[b] = arr[i];
+                            b++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(b);
+            for(int i =0; i < b; i++)
+            {
+                Console.WriteLine(arr1[i]);
+
+            }
+            
+
+
+                
         }
+        
     }
 }
