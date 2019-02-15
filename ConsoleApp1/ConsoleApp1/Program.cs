b79ca9dab79fdb06d5b6ee1f5384e12322643032
tankisov <tankisov.tahir@mail.ru>
2019-02-15 10:58:36 +0600

    qw

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,54 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Task3
 {
     class Program
     {
+        //функция которая выводит число массива 2 раза через пробел
+        static void Print(int n)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write(n + " ");
+            }
+        }
+
         static void Main(string[] args)
         {
-            using System;
-            using System.Collections.Generic;
-            using System.Linq;
-            using System.Text;
-            using System.Threading.Tasks;
-
-namespace Task_2
-    {
-        //я создал класс Student и ввел туда имя, ID и год обучения
-        class Student
-        {
-            public string name;
-            public string ID;
-            public int year;
-
-            //я создал конструктор Student в котором содержится имя и ID
-            public Student(string name, string ID)
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            //cоздаю стринг чтобы вводить в консоле, потом создаю массив стринга и добавляю в него начальный,стринг разделенный пробелами.
+            string s = Console.ReadLine();
+            string[] b = s.Split();
+            //создаю цикл чтобы добавить в массив чисел все числа которые я вводил в виде стринга в интовом виде
+            for (int i = 0; i < b.Length; i++)
             {
-                this.name = name;
-                this.ID = ID;
+                arr[i] = int.Parse(b[i]);
             }
 
-            //Эта функция выводит все элементы класса Student и год обучения увеличивается на 1 год
-            public void Print()
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("Student's name: " + this.name + Environment.NewLine + "Student's ID: " + this.ID + Environment.NewLine + "Student's year of study: " + (this.year + 1));
+                Print(arr[i]);
             }
+            Console.ReadKey();
         }
-
-        class Program
-        {
-
-            static void Main(string[] args)
-            {
-                //Создаю переменную а типа Студент
-                Student a = new Student("Tahir", "18BD110155");
-                a.year = 1;
-                a.Print();
-                Console.ReadKey();
-            }
-        }
-    }
-}
     }
 }
