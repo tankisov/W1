8e9c62f0d42106a26e6cdd0eae71447e10977f78
tankisov <tankisov.tahir@mail.ru>
2019-02-15 10:19:39 +0600

    as

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -4,49 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task1
+namespace Task_2
 {
+    //я создал класс Student и ввел туда имя, ID и год обучения
+    class Student
+    {
+        public string name;
+        public string ID;
+        public int year;
+
+        //я создал конструктор Student в котором содержится имя и ID
+        public Student(string name, string ID)
+        {
+            this.name = name;
+            this.ID = ID;
+        }
+
+        //Эта функция выводит все элементы класса Student и год обучения увеличивается на 1 год
+        public void Print()
+        {
+            Console.WriteLine("Student's name: " + this.name + Environment.NewLine + "Student's ID: " + this.ID + Environment.NewLine + "Student's year of study: " + (this.year + 1));
+        }
+    }
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());// массив из n чисел 
-            int[] arr = new int[n]; // создал новый массив
-            for(int i=0; i < n; i++) 
-            {
-                arr [i] = Convert.ToInt32(Console.ReadLine()); // заполняю массив числами
-            }
-            //-------------------------------------------------
-            int[] arr1 = new int[n];
-            int b = 0;
-            for (int i=0;i<n;i++) // 
-            {
-                int cnt = 0; // создаю каунтер 
-                if (arr[i] > 1)
-                {
-                    for(int j = 2; j < arr[i]; i++) // пробегаюсь по массиву и делю числа на 2
-                    {
-                        if(arr[i] % j != 0) // также создаю условия
-                        {
-                            continue;
-                        }
-                        else {
-                            cnt++;
-                        }
-                        if (cnt == 0)
-                        {
-                            arr1[b] = arr[i];
-                            b++;
-                        }
-                    }
-                }
-            }
-            Console.WriteLine(b); 
-            // пробегаюсь по массиву чтобы вывести количество простых чисел
-            for(int i =0; i < b; i++)
-            {
-                Console.WriteLine(arr1[i]);
-            }     
-        }        
+            //Создаю переменную а типа Студент
+            Student a = new Student("Almaz", "18BD110155");
+            a.year = 1;
+            a.Print();
+            Console.ReadKey();
+        }
     }
 }
